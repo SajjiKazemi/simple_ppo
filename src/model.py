aa2a6c3828b1b99d812b5d9ce5951bea389eabc1
sajji <sajjadkazemi.e@gmail.com>
2023-12-18 16:25:34 -0500

    Create rollout and _init_hyperparameters methods

--- src/model.py
+++ src/model.py
@@ -8,3 +8,22 @@ class PPO:
         self.act_dim = env.action_space.shape[0]
         self.actor = NN(self.obs_dim, self.act_dim)
         self.critic = NN(self.obs_dim, 1)
+        self._initi_hyperparameters()
+
+    def _init_hyperparameters(self):
+        self.timesteps_per_batch = 4800
+        self.max_timesteps_per_episode = 1600
+    
+    def rollout(self):
+        batch_obs = []      # batch observations
+        batch_acts = []     # batch actions
+        batch_log_probs = []        # log probs of each action
+        batch_rews = []     # batch rewards
+        batch_rtgs = []     # batch rewards-to-go
+        batch_lens = []     # episodic lengths in batch
+
+    def learn(self, total_timesteps):
+        t_so_far = 0
+        while t_so_far < total_timesteps:
+            
+    
