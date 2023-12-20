fd4e8cce0dbe778802f3893d645574a4b351a856
sajji <sajjadkazemi.e@gmail.com>
2023-12-20 12:26:16 -0500

    Create the arguments file to handle the arguments

---
+++ src/arguments.py
@@ -0,0 +1,10 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', dest='mode', type=str, default='train', help='train or test')
+    parser.add_argument('--actor_model', dest='actor_model', type=str, default='')
+    parser.add_argument('--critic_model', dest='critic_model', type=str, default='')
+
+    args = parser.parse_args()
+    return args
