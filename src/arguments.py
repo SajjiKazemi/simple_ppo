0c5784fec7843b6a92c256c6d6c727989fbb7728
sajji <sajjadkazemi.e@gmail.com>
2023-12-27 15:27:13 -0500

    Add the render_mode argument to the arguments

--- src/arguments.py
+++ src/arguments.py
@@ -5,6 +5,7 @@ def get_args():
     parser.add_argument('--mode', dest='mode', type=str, default='train', help='train or test')
     parser.add_argument('--actor_model', dest='actor_model', type=str, default='')
     parser.add_argument('--critic_model', dest='critic_model', type=str, default='')
+    parser.add_argument('--render_mode', dest='render_mode', type=str, default='None')
 
     args = parser.parse_args()
     return args
