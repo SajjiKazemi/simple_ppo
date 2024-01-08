d442fa062a20dd74d18e584ddc8f55dbca2c84b1
sajji <sajjikazemi@uwaterloo.ca>
2024-01-08 12:56:38 -0500

    Update the arguments.py to consider the saved updates of the running.

--- src/arguments.py
+++ src/arguments.py
@@ -3,8 +3,8 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', dest='mode', type=str, default='train', help='train or test')
-    parser.add_argument('--actor_model', dest='actor_model', type=str, default='')
-    parser.add_argument('--critic_model', dest='critic_model', type=str, default='')
+    parser.add_argument('--actor_model', dest='actor_model', type=str, default='ppo_actor.pth')
+    parser.add_argument('--critic_model', dest='critic_model', type=str, default='ppo_critic.pth')
     parser.add_argument('--render_mode', dest='render_mode', type=str, default='rgb_array')
 
     args = parser.parse_args()
